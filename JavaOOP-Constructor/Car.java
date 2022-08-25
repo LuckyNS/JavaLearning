e800c088255b9cd49eff15ba7624e681074924ea
Nikolay Spasov <nikolayspasov86@mail.bg>
2022-08-25 15:12:34 +0300

    Add files via upload

---
+++ JavaOOP-Constructor/Car.java
@@ -0,0 +1,10 @@
+public class Car {
+    
+    int modelYear;
+    String modelName;
+    
+    public Car(int year, String model){ // Creating constructor. Name must be same as class
+      modelYear = year;
+      modelName = model;
+    }
+}
---
+++ JavaOOP-Constructor/Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    
+    Car myCar = new Car(2006, "BMW");
+    
+    
+    System.out.println("My car is " + myCar.modelYear + " year and is " + myCar.modelName);
+  }
+}
